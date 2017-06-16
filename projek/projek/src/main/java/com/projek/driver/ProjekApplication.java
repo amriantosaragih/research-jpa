c259d7556e2d297dc3f53cad636dbb3454caa52e
ifs14059 <saragihamrianto@gmail.com>
2017-06-16 17:24:15 +0700

    Add files via upload

---
+++ projek/projek/src/main/java/com/projek/driver/ProjekApplication.java
@@ -0,0 +1,12 @@
+package com.projek.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekApplication.class, args);
+	}
+}
